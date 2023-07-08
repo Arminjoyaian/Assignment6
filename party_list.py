90222f80a35e7ce25d4c91789ca01fadc403960e
Armin joyaian <137637877+Arminjoyaian@users.noreply.github.com>
2023-07-08 21:47:29 +0330

    Delete party_list.py

--- party_list.py
+++
@@ -1,5 +0,0 @@
-names = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
-a = names.append(input("enter name :"))
-b = names.append(input("enter name :"))
-d = names.append(input("enter name :")) 
-print(names)
